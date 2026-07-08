9329a6801a1407c46e114961d2a3f574046134c3
Yulia Svystunova <yu.svystunova@gmail.com>
2026-07-08 19:16:42 +0300

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const iconMenu = document.querySelector('.icon--menu');
+const navLinks = document.querySelectorAll('.nav__link');
+iconMenu.addEventListener('click', () => {
+  document.body.classList.add('page__body--menu-open');
+});
+
+navLinks.forEach ( link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('page__body--menu-open');
+  });
+});
+
