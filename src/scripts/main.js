44cb9e7993ccb5a7af47987faae3cc6f3b9ab38c
Yulia Svystunova <yu.svystunova@gmail.com>
2026-07-10 14:50:11 +0300

    added close item to aside menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@
 
 const iconMenu = document.querySelector('.icon--menu');
 const navLinks = document.querySelectorAll('.nav__link');
+const menuClose = document.querySelector('.menu__close');
+
 iconMenu.addEventListener('click', () => {
   document.body.classList.add('page__body--menu-open');
 });
@@ -12,3 +14,6 @@ navLinks.forEach ( link => {
   });
 });
 
+menuClose.addEventListener('click', () => {
+  document.body.classList.remove('page__body--menu-open');
+});
